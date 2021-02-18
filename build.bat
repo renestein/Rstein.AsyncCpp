0cd771395fac2d0013452a185a6f6292ad118a91
Rene Stein <rene@renestein.net>
2021-02-18 17:35:26 +0100

    iss-23: Added build.bat. (support for building from command line)

---
+++ build.bat
@@ -0,0 +1,103 @@
+@echo off
+set ERROR_NO_VS=-1;
+set ERROR_BAD_OPTIONS=-2;
+set LIB_WIN_CL_STD20_AWAIT=lib_cl_win_std20_await
+set LIB_WIN_CL_LEGACY_AWAIT=lib_cl_win_legacy_await
+set LIB_CLANG_WIN_LEGACY_AWAIT=lib_clang_win_legacy_await
+set CPP_HEADERS_FOLDER=bin\libs\includes
+set RSTEIN_ASYNC_LIB_FOLDER=RStein.AsyncCpp
+
+set TARGET=%1
+if "%TARGET%"=="" set TARGET=%LIB_WIN_CL_STD20_AWAIT%
+if not "%TARGET%"=="%LIB_WIN_CL_STD20_AWAIT%" (
+if not "%TARGET%"=="%LIB_WIN_CL_LEGACY_AWAIT%" (
+if not "%TARGET%"=="%LIB_CLANG_WIN_LEGACY_AWAIT%" goto showusage))
+
+set VS_LOCATION=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\
+echo %VS_LOCATION%
+if not exist "%VS_LOCATION%vcvars32.bat" (
+  echo ERROR. Unable to locate Visual Studio. Exiting.
+  cd %~dp0
+  exit ERROR_NO_VS
+)
+
+echo Preparing build environment. Calling vcvars32.bat.
+call "%VS_LOCATION%vcvars32.bat"
+if %ERRORLEVEL% NEQ 0 goto build_failed
+
+cd %~dp0
+
+
+if %TARGET%==%LIB_WIN_CL_STD20_AWAIT% goto setup_win_cl_std20_await
+if %TARGET%==%LIB_WIN_CL_LEGACY_AWAIT% goto setup_win_cl_legacy_await
+if %TARGET%==%LIB_CLANG_WIN_LEGACY_AWAIT% goto setup_win_clang_legacy_await
+
+:setup_win_cl_std20_await
+set DEBUG_CONFIGURATION=Debug
+set RELEASE_CONFIGURATION=Release
+goto build_solution
+
+:setup_win_cl_legacy_await
+set DEBUG_CONFIGURATION=Debug_VSAWAIT
+set RELEASE_CONFIGURATION=Release_VSAWAIT
+goto build_solution
+
+:setup_win_clang_legacy_await
+set DEBUG_CONFIGURATION=Debug_ClangWin
+set RELEASE_CONFIGURATION=Release_CLangWin
+goto build_solution
+
+:build_solution
+:delete headers
+cd %~dp0
+echo Deleting old header files.
+
+if exist "%CPP_HEADERS_FOLDER%" rmdir /q /s "%CPP_HEADERS_FOLDER%"
+
+:build_projects
+echo Cleaning previous builds.
+Msbuild "RStein.AsyncCppLib.sln" /t:clean
+if %ERRORLEVEL% NEQ 0 goto build_failed
+
+echo Building: Platform=x64 Configuration=%RELEASE_CONFIGURATION%.
+msbuild  "RStein.AsyncCppLib.sln" /p:configuration=%RELEASE_CONFIGURATION% /p:platform=x64 /t:build
+if %ERRORLEVEL% NEQ 0 goto build_failed
+
+echo Building: Platform=x64 Configuration=%DEBUG_CONFIGURATION%.
+msbuild  "RStein.AsyncCppLib.sln" /p:configuration=%DEBUG_CONFIGURATION% /p:platform=x64 /t:build
+
+echo Building: Platform=x86 Configuration=%RELEASE_CONFIGURATION%.
+msbuild  "RStein.AsyncCppLib.sln" /p:configuration=%RELEASE_CONFIGURATION% /p:platform=x86 /t:build
+if %ERRORLEVEL% NEQ 0 goto build_failed
+
+echo Building: Platform=x86 Configuration=%DEBUG_CONFIGURATION%.
+msbuild  "RStein.AsyncCppLib.sln" /p:configuration=%DEBUG_CONFIGURATION% /p:platform=x86 /t:build
+if %ERRORLEVEL% NEQ 0 goto build_failed
+
+:create_headers
+cd %~dp0
+echo Generating header files.
+if not exist "%CPP_HEADERS_FOLDER%" mkdir "%CPP_HEADERS_FOLDER%"
+xcopy "%RSTEIN_ASYNC_LIB_FOLDER%\*.h" "%CPP_HEADERS_FOLDER%" /s /y
+echo.
+echo.
+echo Build succeeded.
+goto end:
+
+:build_failed
+echo.
+echo.
+echo Build failed.
+cd %~dp0
+rem exit %ERRORLEVEL%
+
+:showusage
+echo Usage:
+echo ------
+echo build.bat [target]
+echo target: "%LIB_WIN_CL_STD20_AWAIT%|%LIB_WIN_CL_LEGACY_AWAIT%|%LIB_CLANG_WIN_LEGACY_AWAIT%. Default is %LIB_WIN_CL_STD20_AWAIT%."
+cd %~dp0
+rem exit %ERROR_BAD_OPTIONS%
+
+:end
+rem exit 0
